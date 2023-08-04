9c9ef94a8baa185b21c7f186e7779c51648eadf3
Himanshu Malviya <malviya.himansh@gmail.com>
2023-08-05 01:20:23 +0530

    login with github

--- hosting-website/src/App.jsx
+++ hosting-website/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,10 +8,15 @@ import HomePage from './pages/HomePage'
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddNewProject from './pages/AddNew';
-import StartingPage from './pages/StartingPage'
+import StartingPage from './pages/StartingPage';
+import Callback from './components/callback';
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const queryParams = new URLSearchParams(window.location.search)
+    const code = queryParams.get('code')
+    console.log("This is code param: ", code);
+  }, [])
 
   return (
     <Router>
@@ -21,6 +26,7 @@ function App() {
           <Route path='/' element={<StartingPage />} />
           <Route path='/dashboard' element={<HomePage />} />
           <Route path='/addNew' element={<AddNewProject />} />
+          <Route path='/callback' element={<Callback />} />
         </Routes>
       </main>
     </Router>
--- hosting-website/src/components/Navbar.jsx
+++ hosting-website/src/components/Navbar.jsx
@@ -1,13 +1,15 @@
+import LoginButton from "./LoginButton";
+
 export default function Navbar() {
 
     return (
         <>
             <nav className="backdrop-blur-lg fixed top-0 left-0 right-0 border-b border-b-[var(--primary-light)]">
                 <div className="max-w-5xl flex items-center justify-between py-4 mx-auto">
-                    <h1 className="text-xl font-semibold">🚀</h1>
+                    <h1 className="text-xl font-semibold">ARSync</h1>
                     <div className="flex items-center gap-2">
-                        <button className="px-4 py-2 text-sm hover:bg-[var(--primary-light)]">Log In</button>
-                        <button className="px-4 py-2 text-sm bg-[var(--text)] text-[var(--primary)] border-[var(--text)]">Sign Up</button>
+                        {/* <button className="px-4 py-2 text-sm hover:bg-[var(--primary-light)]">Log In</button> */}
+                        <LoginButton />
                     </div>
                 </div>
             </nav>
