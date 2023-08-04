84fc1c9c76c1a360c534f1c654991062d549503d
rp-ram <ramachakradharrp@gmail.com>
2023-08-04 22:57:51 +0530

    created StartinPage

---
+++ hosting-website/src/pages/StartingPage.jsx
@@ -0,0 +1,47 @@
+import '../styles/StartingPage.css';
+
+function StartingPage() {
+  return (
+    <div className="App">
+      <h3 className='heading'>AR PIPELINE</h3>
+      <p className='text'>Vercel's frontend cloud gives developers the frameworks, 
+        workflows, and infrastructure to build a faster, 
+        more personalized Web</p>
+      <div className='Buttons'>
+        <button className='StartDeploying'>
+          <p>Start Deploying</p>
+        </button>
+        <button className='GetDemo gradient'>
+          <p>Get Demo</p>
+        </button>
+      </div>
+
+      <p className='spacingLetters'>TRUSTED BY THE BEST FRONTEND TEAMS</p>
+      <p className='spacingLetters'>EXPLORE THE VERCEL WAY</p>
+
+      <div className='step-1'>
+        <div className='centre'>
+          <div className='line'></div>
+          <div className='circle'><span className='number'>1</span></div>
+        </div>
+      </div>
+
+      <div className='step-2'>
+        <div className='centre'>
+          <div className='line'></div>
+          <div className='circle'><span className='number'>2</span></div>
+        </div>
+      </div>
+
+      <div className='step-3'>
+        <div className='centre'>
+          <div className='line'></div>
+          <div className='circle'><span className='number'>3</span></div>
+        </div>
+      </div>
+
+    </div>
+  );
+}
+
+export default StartingPage;
