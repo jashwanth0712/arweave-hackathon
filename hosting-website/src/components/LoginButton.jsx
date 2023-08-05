003a9c9539dcaa90818990dc43831858ed5b7adf
Himanshu Malviya <malviya.himansh@gmail.com>
2023-08-06 01:06:16 +0530

    add logout

--- hosting-website/src/components/LoginButton.jsx
+++ hosting-website/src/components/LoginButton.jsx
@@ -11,3 +11,5 @@ export default function LoginButton() {
         <button onClick={handleGitHubLogin} className="px-4 py-2 text-sm bg-[var(--text)] text-[var(--primary)] border-[var(--text)]">Login with GitHub</button>
     );
 }
+
+{/* <button className="px-4 py-2 text-sm hover:bg-[var(--primary-light)]">Log In</button> */}
