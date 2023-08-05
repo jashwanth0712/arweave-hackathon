a5f129347732b1c9e39cfc2a635cf8f08feff270
rp-ram <ramachakradharrp@gmail.com>
2023-08-06 01:20:11 +0530

    changed heading

--- hosting-website/src/pages/StartingPage.jsx
+++ hosting-website/src/pages/StartingPage.jsx
@@ -1,14 +1,16 @@
+import image from '../assets/images/Asset-1.png';
 import '../styles/StartingPage.css';
 
 function StartingPage() {
   return (
     <div className="App">
-      <h3 className='heading'>arsync</h3>
+      <div className='arsync'><img className='image' src={image} alt='Asset-2'/>
+      <h3 className='heading'>arsync</h3></div>
       <p className='vercel'>Vercel's frontend cloud gives developers the frameworks, 
         workflows, and infrastructure to build a faster, 
         more personalized Web</p>
       <div className='Buttons'>
-        <button className='StartDeploying'>
+        <button className='StartDeploying' href= 'https://github.com/login/oauth/authorize?client_id={process.env.clientId}'>
           <p>Start Deploying</p>
         </button>
         <button className='GetDemo'>
@@ -29,14 +31,16 @@ function StartingPage() {
 
       <div className='step-2'>
         <div className='centre'>
-          <div className='line'></div>
+          <div className='line'><p class="firstStep">Hi nice to meet step 1</p>
+        <p class="firstStep">Hi nice to meet step 1</p></div>
           <div className='circle'><span className='number'>2</span></div>
         </div>
       </div>
 
       <div className='step-3'>
         <div className='centre'>
-          <div className='line'></div>
+          <div className='line'><p class="firstStep">Hi nice to meet step 1</p>
+        <p class="firstStep">Hi nice to meet step 1</p></div>
           <div className='circle'><span className='number'>3</span></div>
         </div>
       </div>
