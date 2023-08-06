a42c87e7efba210bffdf503530077b30cc96cb8d
Sasank Madati <sasankmadati@gmail.com>
2023-08-07 00:49:49 +0530

    Added single letter logo

--- hosting-website/src/components/DeployedProjectCard.jsx
+++ hosting-website/src/components/DeployedProjectCard.jsx
@@ -7,7 +7,7 @@ export default function DeployedProjectCard(props) {
         <div className="border border-[var(--primary-light)] hover:border-[var(--text)] rounded-lg h-48 overflow:hidden p-5" >
 
             <div className='flex items-center gap-3'>
-                <div className="h-10 w-10 rounded-full bg-[var(--primary-dark)]"></div>
+                <div className="h-10 w-10 rounded-full bg-[var(--primary-dark)]">{props.data.title[0]}</div>
                 <div>
                     <p className='font-medium text-base'>{props.data.title}</p>
                     <p className='text-sm text-[var(--text-secondary)]'>{props.data.link}</p>
