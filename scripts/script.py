80d35992184c7d0e1507ea178f8b85e81f46ff0d
jashwanth0712 <jashwanth0712@gmail.com>
2023-08-07 23:04:21 +0530

    added upload folder function

--- scripts/script.py
+++ scripts/script.py
@@ -1,28 +1,59 @@
 import subprocess
 import json
 import argparse
-def get_drive_list(wallet_address):
-    command = f'ardrive list-drive -w {wallet_address} -d {args.drive}'
+def build_folder(root):
+    command=f'ardrive upload-file -l "{args.build}_{args.commit}" -w {args.wallet} --parent-folder-id {root}'
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
-    return result.stdout
-def get_build_file():
-    cli_output = get_drive_list(args.wallet)
-    print(cli_output)
+
+def get_root_folder_id():
+    cli_output=get_drive_list()
     try:
         parsed_data = json.loads(cli_output)
         for i in parsed_data:
-            if i['name'][:5] == "build":
-                return i
+            if i['path'] == "/test":
+                print(">>>",i['folderId'])
+                return i['folderId']
     except json.JSONDecodeError as e:
         print("Error parsing JSON:", e)
 
+def get_build_file():
+    cli_output=get_drive_list()
+    try:
+        parsed_data = json.loads(cli_output)
+        for i in parsed_data:
+            if i['name'] == "build"+"_"+args.commit:
+                return i['folderId']
+    except json.JSONDecodeError as e:
+        print("Error parsing JSON:", e)
+def get_drive_list():
+    command = f'ardrive list-drive -w {args.wallet} -d {args.drive}'
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
+    return result.stdout
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Find and display object with a specified name from arweave list-drive output.')
     parser.add_argument('-w', '--wallet', type=str, required=True, help='Wallet address')
     parser.add_argument('-d', '--drive', type=str, required=True, help='drive address')
+    parser.add_argument('-c', '--commit', type=str, required=True, help='commit number')
+    parser.add_argument('-b', '--build', type=str, required=True, help='build file root')
     args = parser.parse_args()
-    get_build_file()
+    # user creates wallet and a drive with name arsync
+    # get_drive_list()
+
+    # getting the folder id of root
+    # root_folder_id=get_root_folder_id()
+
+    # uploading build file with name build_<commit number>
+    # build_folder(root_folder_id)
+
+    # getting the uploaded build folder details
+    # current_build_folder_id=get_build_file()
+
+    # creating a manifest and returning the link
+    
+
+    
+
 
 
     
