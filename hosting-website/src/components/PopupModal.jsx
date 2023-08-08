f33371e8440b2914fcc2d32b12ec3676d075fa2f
rp-ram <ramachakradharrp@gmail.com>
2023-08-08 21:32:18 +0530

    slideshow in popup modal

--- hosting-website/src/components/PopupModal.jsx
+++ hosting-website/src/components/PopupModal.jsx
@@ -1,8 +1,48 @@
-import React from "react";
-import {Carousel,initTE,} from "tw-elements"
+import React, { useState, useEffect } from "react";
+
+const imageLinks = [
+  "https://imgs.search.brave.com/gZSPHKpSrfrpTkAUsGXiZo-i3WLr8IKrDmL4Ia2KHOw/rs:fit:500:0:0/g:ce/aHR0cHM6Ly93d3cu/ZGlnaXRhbG9jZWFu/LmNvbS9fbmV4dC9z/dGF0aWMvbWVkaWEv/aW50cm8tdG8tY2xv/dWQuZDQ5YmM1Zjcu/anBlZw",
+  "https://imgs.search.brave.com/_RtpHldsu_U1uPCUT-8j93uhPyK3zl0Gf1aDnFzYjdA/rs:fit:500:0:0/g:ce/aHR0cHM6Ly9taXJv/Lm1lZGl1bS5jb20v/djIvMSo3SWFDTnVl/WjRxcjRHTXFVdGNW/WVdRLnBuZw",
+
+  // Add more image links as needed
+];
 
 export default function PopupModal() {
   const [showModal, setShowModal] = React.useState(false);
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    let imageInterval;
+
+    if (showModal) {
+      imageInterval = setInterval(() => {
+        setCurrentImageIndex((prevIndex) => (prevIndex + 1) % imageLinks.length);
+      }, 2000);
+    } else {
+      clearInterval(imageInterval);
+    }
+
+    return () => {
+      clearInterval(imageInterval);
+    };
+  }, [showModal]);
+
+  useEffect(() => {
+    let closeTimeout;
+  
+    if (showModal) {
+      closeTimeout = setTimeout(() => {
+        setShowModal(false);
+      }, 6000);
+    } else {
+      clearTimeout(closeTimeout);
+    }
+  
+    return () => {
+      clearTimeout(closeTimeout);
+    };
+  }, [showModal]);
+  
   return (
     <>
       <button className="GetDemo" onClick={() => setShowModal(true)}>
@@ -15,127 +55,34 @@ export default function PopupModal() {
           className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none backdrop-blur-md"
         >
           <div className="relative w-auto my-6 mx-auto max-w-3xl">
-            {/*content*/}
-            <div className="border rounded-lg shadow-lg relative flex flex-col w-full bg-transparent backdrop-filter backdrop-blur-md">
-              {/*header*/}
-              <div className="flex items-start justify-between p-5   rounded-t">
-                <h3 className="text-3xl font-semibold">
-                  Slide Show
-                </h3>
-                <button
-                  className="p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none"
-                  onClick={() => setShowModal(false)}
-                >
-                  <span className="bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none">
-                    ×
-                  </span>
-                </button>
+            {/* {/content/} */}
+            <div className="rounded-lg shadow-lg relative flex flex-col w-full bg-transparent backdrop-filter backdrop-blur-md">
+              {/* {/header/} */}
+              <div className="flex items-start justify-between p-0 rounded-t">
+          
               </div>
-              {/*body*/}
-              <div
-              id="carouselExampleControls"
-              class="relative"
-              data-te-carousel-init
-              data-te-ride="carousel">
-
-              <div
-                class="relative w-full overflow-hidden after:clear-both after:block after:content-['']">
-
-                <div
-                  class="relative float-left -mr-[100%] w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                  data-te-carousel-item
-                  data-te-carousel-active>
+              {/* {/body/} */}
+              <div className="relative p-6 flex-auto ">
                   <img
-                    src="https://mdbcdn.b-cdn.net/img/new/slides/041.webp"
-                    class="block w-full"
-                    alt="Wild Landscape" />
-                </div>
-
-                <div
-                  class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                  data-te-carousel-item>
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/new/slides/042.webp"
-                    class="block w-full"
-                    alt="Camera" />
-                </div>
-
-                <div
-                  class="relative float-left -mr-[100%] hidden w-full transition-transform duration-[600ms] ease-in-out motion-reduce:transition-none"
-                  data-te-carousel-item>
-                  <img
-                    src="https://mdbcdn.b-cdn.net/img/new/slides/043.webp"
-                    class="block w-full"
-                    alt="Exotic Fruits" />
-                </div>
+                    src={imageLinks[currentImageIndex]}
+                    alt={`Slide ${currentImageIndex}`}
+                    className="w-full rounded-lg"
+                  />
               </div>
-              
-  <button
-    class="absolute bottom-0 left-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-    type="button"
-    data-te-target="#carouselExampleControls"
-    data-te-slide="prev">
-    <span class="inline-block h-8 w-8">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="h-6 w-6">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M15.75 19.5L8.25 12l7.5-7.5" />
-      </svg>
-    </span>
-    <span
-      class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-      >Previous</span
-    >
-  </button>
   
-  <button
-    class="absolute bottom-0 right-0 top-0 z-[1] flex w-[15%] items-center justify-center border-0 bg-none p-0 text-center text-white opacity-50 transition-opacity duration-150 ease-[cubic-bezier(0.25,0.1,0.25,1.0)] hover:text-white hover:no-underline hover:opacity-90 hover:outline-none focus:text-white focus:no-underline focus:opacity-90 focus:outline-none motion-reduce:transition-none"
-    type="button"
-    data-te-target="#carouselExampleControls"
-    data-te-slide="next">
-    <span class="inline-block h-8 w-8">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="h-6 w-6">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-      </svg>
-    </span>
-    <span
-      class="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"
-      >Next</span
-    >
-  </button>
-</div>
-              {/*footer*/}
-              <div className="flex items-center justify-end p-6 rounded-b">
-                <button
-                  className="text-white-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+              <button
+                  className="flex text-white-500 background-transparent font-bold uppercase px-6 py-2 w-fit justify-center text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                   type="button"
                   onClick={() => setShowModal(false)}
                 >
-                  Close
-                </button>
-              </div>
+                  Skip
+              </button>
             </div>
           </div>
         </div>
       </>
       
       ) : null}
-    </>
-  );
+    </>
+  );
 }
